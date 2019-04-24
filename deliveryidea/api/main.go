768ec63572a5b5fbfb4a824f7d6ac5e2f5f12c74
Bimesh De Silva <bimesh.de.silva@sap.com>
2019-04-24 19:00:47 -0400

    Changing number

--- deliveryidea/api/main.go
+++ deliveryidea/api/main.go
@@ -57,7 +57,7 @@ func registerOrderService(r *gin.Engine, conn *grpc.ClientConn) {
 				CustomerId:       fmt.Sprintf("%s_%d", name, time.Now().Unix()),
 				Name:             name,
 				DeliveryLocation: "H3L1L2",
-				Phone:            "+14164545447"},
+				Phone:            "+12262203524"},
 		}
 		if res, err := client.PlaceOrder(c, body); err == nil {
 			c.JSON(http.StatusOK, gin.H{
